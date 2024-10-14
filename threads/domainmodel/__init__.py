ed65b8a30db147ed9c9844eb9449dbc8db89a972
zli775 <qq2822856916@outlook.com>
2024-10-14 18:45:38 +1300

    Clean

--- threads/domainmodel/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import Tag, Thread, User, Review, Favorite
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-
-from threads import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=5000, threaded=False,debug=True)
